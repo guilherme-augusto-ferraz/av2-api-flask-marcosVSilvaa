be47a81cb9ab698cecdef5cb21a74990ce34b7bb
Gisella Garcia <garciaggmiranda77@gmail.com>
2025-11-28 22:58:17 +0000

    g3

--- models/task.py
+++ models/task.py
@@ -6,10 +6,25 @@ class Task(db.Model):
     __tablename__ = 'tasks'
     
     id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255))
     status = db.Column(db.String(20), default='pendente', nullable=False)
-    due_date = db.Column(db.Date, nullable=False, default=datetime.utcnow().date())
+    due_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
-    # Chave Estrangeira: user_id (Requisito de associação)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+# Chave Estrangeira: user_id (Requisito de associação)
+user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    def __repr__(self):
+        ` return f'<Task {self.title}
+
+def to_dict(self):
+	return {
+	'id': self.id,
+	'title': self.title,
+	'description': self.description,
+	'status': self.status,
+	'due_date': self.due_date.isoformat(),
+	'user_id': self.user_id
+	# Se você adicionou created_at:
+	# 'created_at': self.created_at.isoformat()
+	}
