f8b6b1f2827c519722b876ca8cbef9852992acb2
Marcos Vinícius <marcosvinisilv4@gmail.com>
2025-11-28 18:39:33 +0000

    etapa3

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # app.py
 from flask import Flask
-from database import configure_database, db
+from database import configure_database, db # Importa db
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 import os
 
-# 1. Carrega as variáveis de ambiente do arquivo .env
+# 1. Carrega as variáveis de ambiente (.env)
 load_dotenv()
 
 # Inicialização da Aplicação
@@ -18,19 +18,26 @@ jwt = JWTManager(app)
 # 3. Configuração e Inicialização do Banco de Dados
 configure_database(app)
 
-# 4. Importação e Registro das Rotas (faremos isso na próxima etapa)
-# from routes.users import users_bp
-# app.register_blueprint(users_bp, url_prefix='/api/users')
-# ...
+# 4. Importação e Registro das Rotas (Etapas 3 e 4)
+# Importa o Blueprint das rotas de usuário
+from routes.users import users_bp 
+# from routes.tasks import tasks_bp # Será usado na Etapa 4
+
+# Registra o Blueprint das rotas de usuário.
+app.register_blueprint(users_bp, url_prefix='/api/users')
+# app.register_blueprint(tasks_bp, url_prefix='/api/tasks') # Será usado na Etapa 4
 
 @app.route('/')
 def hello_world():
-    return 'API de Gerenciamento Financeiro está online!'
+    return 'API de Lista de Tarefas está online!'
 
 if __name__ == '__main__':
-    # Importe os modelos aqui para garantir que o db.create_all() os encontre
+    # Importa os modelos para que o SQLAlchemy saiba quais tabelas criar
     from models.user import User
-    # Mudança aqui:
-    from models.task import Task
+    from models.task import Task 
+    
+    # Cria as tabelas no BD (users e tasks), se não existirem
+    with app.app_context():
+        db.create_all() 
     
     app.run(debug=True)
