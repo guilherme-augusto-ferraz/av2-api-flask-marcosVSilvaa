c3d5c87dc6578bae6e34801aa0f75cc510997379
Gisella Garcia <garciaggmiranda77@gmail.com>
2025-11-28 22:38:25 +0000

    Melhoria database

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 # database.py
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import os
 
 db = SQLAlchemy()
 
 def configure_database(app: Flask):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "DATABASE_URL", 
+        "sqlite:///app.db"
+    ) 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     # A linha abaixo deve ser executada para criar o banco de dados na primeira vez
     # with app.app_context():
-    #     db.create_all()
+    #      db.create_all()
